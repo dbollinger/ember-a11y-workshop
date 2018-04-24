ab91b477bf486dac65fb374f454cd78e1c2fe5a6
howie <db@redshelf.com>
2018-04-24 16:23:27 -0500

    Clean up first exercise and add scaffolding for other exercises.

--- app/components/user-profile.js
+++ app/components/user-profile.js
@@ -1,5 +1,12 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  classNames: ['user-profile']
+  classNames: ['user-profile'],
+  classNameBindings: ['isSelected:is-selected'],
+
+  actions: {
+    selectUser(){
+      this.set('isSelected', true);
+    }
+  }
 });
