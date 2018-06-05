5f0e662c86b8da4cc1a155b365dcd310f8ab652b
howie <db@redshelf.com>
2018-06-05 09:58:44 -0500

    - Some small adjustments to existing exercises

--- tests/acceptance/color-contrast-test.js
+++ tests/acceptance/color-contrast-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { visit } from '@ember/test-helpers';
+import { visit, find, click } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import a11yAudit from 'ember-a11y-testing/test-support/audit';
 
@@ -11,6 +11,16 @@ module('Acceptance | Exercise | color contrast', function(hooks) {
 
     let axeOptions = {};
     await a11yAudit(axeOptions);
+    assert.ok(true, 'no a11y errors found!');
+
+    await click('.disable-brands');
+
+    await a11yAudit(axeOptions);
+    assert.ok(true, 'no a11y errors found!');
+
+    let firstLogo = await find('.brand-logo:first-of-type');
+    firstLogo.focus();
+
     assert.ok(true, 'no a11y errors found!');
   });
 });
